0071f3a011a623bfd43c4d5bccb59abdb934cce3
Arjun Chandran <arjun_chandran@berkeley.edu>
2021-04-16 13:10:59 -0700

    Trying to fix some systemic bugs regarding our datastructure

--- Cheminformatics/tests/org/ucb/act/ROProjection/ROUtilsTests.java
+++ Cheminformatics/tests/org/ucb/act/ROProjection/ROUtilsTests.java
@@ -12,16 +12,40 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.ucb.act.utils.ChemAxonUtils;
-
+import org.ucb.act.ro.ROUtils;
 /**
  *
  * @author Arjun Chandran
  */
 
 public class ROUtilsTests{
-
+    ///Beta-tests
     @Test
     public void explode_test() throws Exception{
+        Parser parser = new Parser();
+        ROUtils utils = new ROUtils();
+        Set<String> namesROs = parser.RORun("./2015_01_16-ROPruner_hchERO_list.txt");
+        HashMap<String, HashMap> explodedROs = new HashMap<>();
+        for(String ro: namesROs){
+            print(ro);
+            HashMap<String, Molecule[]> rxn = utils.explode(ro);
+            explodedROs.add(ro, rxn);
 
+        }
+        print(explodedROs);
+    }
+
+    @Test
+    public void get_mass_difference_test() throws Exception{
+        ROUtils utils = new ROUtils();
+        Parser parser = new Parser()
+        Set<String> namesROs = parser.RORun("./2015_01_16-ROPruner_hchERO_list.txt");
+        for(String ro: namesROs){
+            print(ro);
+            double mass_diff = utils.get_mass_difference(ro)
+            explodedROs.add(ro, mass_diff);
+
+        }
+        print(explodedROs);
     }
 }
